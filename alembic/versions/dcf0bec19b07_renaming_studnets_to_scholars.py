23e4d795620a8c46ba8b082009f3a43c19e24bd3
danwycliff <danwycliff6769@gmail.com>
2023-09-30 05:52:21 +0300

    codes

---
+++ alembic/versions/dcf0bec19b07_renaming_studnets_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming studnets to scholars
+
+Revision ID: dcf0bec19b07
+Revises: 
+Create Date: 2023-09-29 22:53:23.866577
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dcf0bec19b07'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
